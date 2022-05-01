b8420b16c1ce45169e0684f295762e900e721451
DokiDokiPB <DokiDokiPB@githubnoreply.com>
2022-05-01 13:24:19 +0800

    Implement function new and free

--- decnum.c
+++ decnum.c
@@ -1,1 +1,34 @@
 #include "decnum.h"
+void decnum_new(decnum_t *ptr, uint32_t size)
+{
+    if (!ptr) {
+        return;
+    }
+    ptr->digits = malloc(sizeof(uint32_t) * size);
+}
+
+void decnum_free(decnum_t *ptr)
+{
+    if (!ptr) {
+        return;
+    }
+    free(ptr->digits);
+}
+
+void decnum_add(const decnum_t *b1, const decnum_t *b2, decnum_t *result) {
+    if(!result && !b1 && !b2){
+        return;
+    }
+}
+
+void decnum_sub(const decnum_t *b1, const decnum_t *b2, decnum_t *result) {
+    if(!result && !b1 && !b2){
+        return;
+    }
+}
+
+void decnum_mult(const decnum_t *b1, const decnum_t *b2, decnum_t *result) {
+    if(!result && !b1 && !b2){
+        return;
+    }
+}
